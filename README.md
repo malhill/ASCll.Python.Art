ed5144f3e27e02ddd45cf607a0ff8f5553c054cd
malhill <malcolmhc@msn.com>
2021-09-17 14:25:12 -0400

    first and last initials

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ASCll.Python.Art
+
+* Practicing using block letters
+* Information about ASCII art: [Wiki Page]("https://en.wikipedia.org/wiki/ASCII_art")
---
+++ initials.py
@@ -0,0 +1,25 @@
+#Malcolm Hill
+first_initial='''
+M     M
+MM   MM
+MM   MM
+M  M  M
+M     M
+M     M
+M     M
+'''
+
+second_initial='''
+H   H
+H   H
+H   H
+HHHHH
+H   H
+H   H
+H   H
+'''
+
+print(first_initial, second_initial)
+
+#I like to play video games
+print()
